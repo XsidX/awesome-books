5c59a9117ed29fd8589f977622ae6e87f662b94a
iamsid <developer.sid@outlook.com>
2022-06-29 22:47:51 +0300

    add active link, add dynamic date, fix linting errors

--- awesomeBooks.js
+++ awesomeBooks.js
@@ -2,6 +2,47 @@ import Book from './Book.js';
 import BookStore from './BookStore.js';
 import UI from './UI.js';
 
+const bookList = document.querySelector('#view-book');
+const form = document.querySelector('#add-book');
+const contact = document.querySelector('#contact');
+
+const listLink = document.querySelector('.list-book');
+const addBookLink = document.querySelector('.add-new');
+const contactLink = document.querySelector('.contact-link');
+
+listLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  listLink.classList.add('active');
+  addBookLink.classList.remove('active');
+  contactLink.classList.remove('active');
+
+  bookList.className = 'book-list-wrapper display-block';
+  form.className = 'display-none';
+  contact.className = 'display-none';
+});
+
+addBookLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  listLink.classList.remove('active');
+  addBookLink.classList.add('active');
+  contactLink.classList.remove('active');
+
+  form.className = 'display-flex content-container';
+  bookList.className = 'display-none';
+  contact.className = 'display-none';
+});
+
+contactLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  listLink.classList.remove('active');
+  addBookLink.classList.remove('active');
+  contactLink.classList.add('active');
+
+  contact.className = 'display-flex';
+  form.className = 'display-none';
+  bookList.className = 'display-none';
+});
+
 // Event: Add Button
 document.querySelector('#form').addEventListener('submit', (e) => {
   // Prevent default submission
@@ -13,7 +54,7 @@ document.querySelector('#form').addEventListener('submit', (e) => {
 
   // Validate
   if (title === '' || author === '') {
-    UI.showAlert('Please fill in all fields', 'alert');
+    UI.showAlert('Please fill in all fields', 'error');
   } else {
     const book = new Book(title, author);
 
@@ -22,6 +63,8 @@ document.querySelector('#form').addEventListener('submit', (e) => {
     BookStore.addBookToStore(book);
 
     UI.clearFields();
+
+    UI.showAlert('Book added', 'success');
   }
 });
 
@@ -38,3 +81,16 @@ document.querySelector('.container').addEventListener('click', (e) => {
   const title = e.target.previousElementSibling.previousElementSibling.textContent.replace(/[^a-z0-9]/gi, '');
   BookStore.removeBook(title);
 });
+
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December',
+];
+const dateObj = new Date();
+const month = monthNames[dateObj.getUTCMonth() + 1]; // months from 1-12
+const day = dateObj.getUTCDate();
+const year = dateObj.getUTCFullYear();
+
+const newdate = `${month} ${day} ${year}`;
+const time = new Date().toLocaleTimeString().toLowerCase();
+
+document.querySelector('.date').textContent = `${newdate}, ${time}`;
